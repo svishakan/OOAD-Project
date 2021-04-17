3622b013970c314d5bd5d2a91ac984d51cee3933
Venkataraman-Nagarajan <njvraman@gmail.com>
2021-04-18 00:32:59 +0530

    Added Dashboard

--- quiz-app/src/components/dashboard.js
+++ quiz-app/src/components/dashboard.js
@@ -1,37 +1,51 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+
 import "./dashboard.css";
 
 function DashBoard(props) {
-    const [handle, setHandle] = useState("");
-    
-    useEffect(() => {
-    setHandle(props.location.state.handle);
-    }, [props.location.state.handle]);
+  const [handle, setHandle] = useState("");
+  const [redirectHome, setRedirectHome] = useState(false);
+  useEffect(() => {
+    if (
+      props.location.state === undefined ||
+      props.location.state.handle === undefined
+    ) {
+      setRedirectHome(true);
+    } else {
+      setHandle(props.location.state.handle);
+    }
+  }, [props.location.state]);
 
   return (
-    <div className="fixed top-0 left-0 justify-center w-screen h-screen">
-      <div>
-          {handle}
-      </div>
-      <table>
-        <tr>
-          <td>
-            <button onClick="take_quiz();">Take up a Quiz</button>
-          </td>
-          <td>
-            <button onClick="create_quiz();">Create a Quiz</button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button onClick="view_profile();">View Profie</button>
-          </td>
-          <td>
-            <button onClick="();"></button>
-          </td>
-        </tr>
-      </table>
+    <div>
+      {redirectHome === true ? (
+        <Redirect to="/enter" />
+      ) : (
+        <div className="fixed top-0 left-0 justify-center w-screen h-screen">
+          <div>{handle}</div>
+          <table>
+            <tr>
+              <td>
+                <button onClick="take_quiz();">Take up a Quiz</button>
+              </td>
+              <td>
+                <button onClick="create_quiz();">Create a Quiz</button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button onClick="view_profile();">View Profie</button>
+              </td>
+              <td>
+                <button onClick="();"></button>
+              </td>
+            </tr>
+          </table>
+        </div>
+      )}
+      ;
     </div>
   );
 } /*
