8c9bf211fdb13de070308c4678f075169b257ad7
Vikram <vikramvenkat2408@gmail.com>
2021-04-26 20:32:08 +0530

    Improved dashboard

--- quiz-app/src/components/dashboard.js
+++ quiz-app/src/components/dashboard.js
@@ -1,151 +1,170 @@
 import React from "react";
 import { useState, useEffect } from "react";
-//import Loading from "./loading";
 import { Redirect } from "react-router-dom";
-//import 'bootstrap/dist/css/bootstrap.css';
+import {
+  Button,
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+} from "react-bootstrap";
 
 import Toaster from "./toaster";
-
-//import CreateQuiz from "/components/crearequiz";
-//import TakeUpQuiz from "/components/takeupquiz";
-//import ViewProfile from "/components/viewprofile";
+import UserPic from "../images/user.svg";
 
 import "./dashboard.css";
+import "./about.css";
 import { render } from "@testing-library/react";
 
 function DashBoard() {
-    const [handle, setHandle] = useState("");
-    const [redirectHome, setRedirectHome] = useState(false);
-    const [redirectTake, setRedirectTake] = useState(false);
-    const [redirectSet, setRedirectSet] = useState(false);
-    const [redirectReport, setRedirectReport] = useState(false);
-    //const [redirectHome, setRedirectHome] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [handle, setHandle] = useState("");
+  const [redirectHome, setRedirectHome] = useState(false);
+  const [redirectTake, setRedirectTake] = useState(false);
+  const [redirectSet, setRedirectSet] = useState(false);
+  const [redirectReport, setRedirectReport] = useState(false);
+  // const [redirectFeedback, setredirectFeedback] = useState(false);
 
-    let myStorage = window.localStorage;
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        if (myStorage.getItem("handle") === handle) {
-            setRedirectHome(true);
-        } else {
-            setHandle(myStorage.getItem("handle"));
-        }
-    }, []);
+  let myStorage = window.localStorage;
 
-    if (redirectSet) return <Redirect to="/quizcreator" />;
-    if (redirectTake) return <Redirect to="/quizfinder" />;
-    if (redirectReport) return <Redirect to="/YourQuizes" />;
-    return (
+  // useEffect(() => {
+  //   if (myStorage.getItem("handle") === handle) {
+  //     setRedirectHome(true);
+  //   } else {
+  //     setHandle(myStorage.getItem("handle"));
+  //   }
+  // }, []);
+
+  if (redirectSet) return <Redirect to="/quizcreator" />;
+  if (redirectTake) return <Redirect to="/quizfinder" />;
+  if (redirectReport) return <Redirect to="/YourQuizes" />;
+  //if (redirectFeedback) return <Redirect to="/Feedback" />;
+
+  return (
+    <div>
+      {redirectHome === true ? (
+        <Redirect to="/enter" />
+      ) : (
         <div>
-            {redirectHome === true ? (
-                <Redirect to="/enter" />
-            ) : (
-                <div className="justify-items-center rounded-lg shadow-lg py-6 px-6 m-auto  bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 align-middle">
-                    <div className="flex justify-end right absolute right-10">
-
-
-                        <nav
-                            className="navbar navbar-expand-lg navbar-light bg-light"
-                            style={{ backgroundColor: "#e3f2fd" }}>
-                            <div className="container-fluid">
-                                <a className="navbar-brand" href="#">
-                                    Hi, {myStorage.getItem("handle")}
-                                </a>
-                                <button
-                                    className="navbar-toggler"
-                                    type="button"
-                                    data-toggle="collapse"
-                                    data-target="#navbarNavAltMarkup"
-                                    aria-controls="navbarNavAltMarkup"
-                                    aria-expanded="false"
-                                    aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
-                                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                    <div className="navbar-nav">
-                                        <a className="nav-item nav-link active" href="#">
-                                            Profile <span className="sr-only">(current)</span>
-                                        </a>
-                                        <a className="nav-item nav-link" href="#">
-                                            Logout
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </nav>
-                        <button
-                            className="logout"
-                            onClick={() => {
-                                console.log("of to login");
-                                render(
-                                    <Toaster 
-                                        headerText={"Logged Out"} 
-                                        bodyText={`${handle} successfully logged out!`}
-                                        bgType={"bg-info"}
-                                        textColor={"text-white"} />
-                                );
-                                setRedirectHome(true);
-                            }}>
-                            Logout</button>
-                    </div>
-                    <div className="fixed pt-20">
-                        <div className="flex absolute justify-around w-screen ">
-                            <button
-                                className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                                onClick={() => {
-                                    setRedirectTake(true);
-                                }}>
-                                Take Up Quiz</button>
-
-                            <button
-                                className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                                onClick={() => {
-                                    setRedirectSet(true);
-                                }}>Create a Quiz</button>
-
-                            <button
-                                className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                                onClick={() => { }}>
-                                View Profile</button>
-
-                            <button
-                                className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                                onClick={() => {
-                                    setRedirectReport(true);
-                                }}>
-                                Quiz Reports</button>
-                        </div>
+          <div className="justify-items-center rounded-lg shadow-lg py-6 px-6 m-auto  bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 align-middle">
+            <div className="flex justify-end right absolute right-10">
+              <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand>
+                  Wecome, {myStorage.getItem("handle")}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="navbar-nav ml-auto">
+                    <img class="user" height="50" src={UserPic} />
+                    <div class="">
+                      <div className="card-img">
+                      <i class="fa fa-user-circle-o fa-3x" aria-hidden="true"></i>
                     </div>
 
-                    {/* <table>
-          <div className="flex items-center">
-            <tr>
-              <td>
-              
-              <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded transform motion-safe:hover:scale-110">
-                Take Up Quiz</button>
-              </td>
-              <td>
-              <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-                Create a Quiz</button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-              <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-              View Profie</button>
-              </td>
-              <td>
-              <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-               </button>
-              </td>
-            </tr>
+                      <NavDropdown
+                        title={myStorage.getItem("handle")}
+                        id="collasible-nav-dropdown"
+                      >
+                        <NavDropdown.Item href="#profile">
+                          Edit Profile
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="/Feedback">
+                          Got a feedback?
+                        </NavDropdown.Item>
+
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/About">
+                          About Us
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="/enter">
+                          <div className="card-img">
+                            <i class="fa fa-sign-out" aria-hidden="true" />
+                            Logout
+                          </div>
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                    </div>
+                    <div className="flex justify-right-10">
+                      <Navbar.Brand>
+                        <picture
+                          src="/src/images/.svg"
+                          width="30"
+                          height="30"
+                          className="d-inline-block align-top"
+                          alt="logo"
+                        />
+                      </Navbar.Brand>
+                    </div>
+                  </Nav>
+                </Navbar.Collapse>
+              </Navbar>
             </div>
-          </table> */}
+            <div className="">
+              <div className="card-deck card-custom-margin">
+                <div className="card about-box">
+                  <h1 className="card-header2 ">Take Up a Quiz</h1>
+                  <div className="card-body">
+                    <p className="card-text text-white card-p-text">
+                      Got your 'QuizID'? Ready to take a Quiz? Then hit that GO
+                      button!!
+                    </p>
+                    <button
+                      class="btn btn-primary"
+                      className="btn btn-qf-neon-primary text-nowrap"
+                      onClick={() => {
+                        setRedirectTake(true);
+                      }}
+                    >
+                      GO
+                    </button>
+                  </div>
                 </div>
-            )}
+
+                <div className="card about-box">
+                  <h1 className="card-header2">Create a Quiz</h1>
+                  <div className="card-body">
+                    <p className="card-text text-white card-p-text">
+                      Create you own quiz, share the QuizID to let others
+                      take the quiz.
+                    </p>
+                    <button
+                      class="btn btn-warning"
+                      className="btn btn-qf-neon-primary text-nowrap"
+                      onClick={() => {
+                        setRedirectSet(true);
+                      }}
+                    >
+                      Create
+                    </button>
+                  </div>
+                </div>
+
+                <div className="card about-box">
+                  <h1 className="card-header2">View Report</h1>
+                  <div className="card-body">
+                    <p className="card-text text-white card-p-text">
+                      View the report(s) of the quizes you created.
+                    </p>
+                    <button
+                      class="btn btn-success"
+                      className="btn btn-qf-neon-primary text-nowrap"
+                      onClick={() => {
+                        setRedirectReport(true);
+                      }}
+                    >
+                      View
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default DashBoard;
