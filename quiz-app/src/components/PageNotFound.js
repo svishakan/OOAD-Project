5e32258b7d7fc9c41961b3c25e353548c343a315
Vishakan Subramanian <svishakan123@gmail.com>
2021-04-28 20:24:34 +0530

    Fixed PageNotFound bug :bug:

--- quiz-app/src/components/PageNotFound.js
+++ quiz-app/src/components/PageNotFound.js
@@ -1,26 +1,20 @@
-import Navigation from "./Navigation";
-
 function PageNotFound() {
     return (
-        <div>
-            <Navigation />
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-lg-6 col-md-8 login-box">
-                        <div className="col-lg-12 login-img">
-                            <i class="fas fa-bug"></i>
-                        </div>
-                        <h1 className="col-lg-12 col-md-12 col-sm-12 quiz-title text-center">
-                            ERROR: 404
-                        </h1>
-                        <h1 className="col-lg-12 col-md-12 col-sm-12 quiz-title text-center">
-                            Page not found
-                        </h1>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-lg-6 col-md-8 login-box">
+                    <div className="col-lg-12 login-img">
+                        <i class="fas fa-bug"></i>
                     </div>
+                    <h1 className="col-lg-12 col-md-12 col-sm-12 quiz-title text-center">
+                        ERROR: 404
+                        </h1>
+                    <h1 className="col-lg-12 col-md-12 col-sm-12 quiz-title text-center">
+                        Page not found
+                        </h1>
                 </div>
             </div>
         </div>
-
     );
 }
 
